09be8fc5ccc82f2121fdb5d90ce696f8e69b173f
Igor <coalenergy84@ukr.net>
2021-11-18 15:25:34 +0200

    OneAndFiveSecond: -used  wait() and notifyAll() methods.

--- src/main/java/OneAndFiveSecond.java
+++ src/main/java/OneAndFiveSecond.java
@@ -1,33 +1,47 @@
 public class OneAndFiveSecond {
-    static int time = 0;
+
+    static int count = 0;
     static boolean flag = true;
 
+    static class ForSynchronize {
+    }
+
     public static void main(String[] args) {
-        Thread fiveSeconds = new Thread(new Runnable() {
-            @Override
-            public synchronized void run() {
-                while (flag) {
-                    try {
-                        Thread.sleep(5000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                    System.out.println("Прошло 5 секунд");
-                }
-            }
 
-        });
+        OneAndFiveSecond oneAndFiveSecond = new OneAndFiveSecond();
 
+        Thread timer = new Thread(oneAndFiveSecond::oneSecond);
+        Thread fiveSeconds = new Thread(oneAndFiveSecond::fiveSeconds);
+
+        timer.start();
         fiveSeconds.start();
+    }
 
+    public void oneSecond() {
         while (flag) {
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            time++;
-            System.out.println(time + " seconds passed from the beginning");
+            synchronized (ForSynchronize.class) {
+                count++;
+                System.out.println(count + " seconds passed from the beginning");
+                ForSynchronize.class.notifyAll();
+            }
+        }
+    }
+
+    public void fiveSeconds() {
+        try {
+            while (flag)
+                synchronized (ForSynchronize.class) {
+                    ForSynchronize.class.wait();
+                    if (count % 5 == 0)
+                        System.out.println(("Прошло 5 секунд"));
+                }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 }
